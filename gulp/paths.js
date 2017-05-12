842006b95fc3843ea83ed31c2e595dd99c858b30
brenopolanski <breno.polanski@gmail.com>
2017-05-12 17:17:04 -0300

    Update task vscode

--- gulp/paths.js
+++ gulp/paths.js
@@ -4,7 +4,15 @@ module.exports = {
   toolkit: {
     vscodeIntellisense: {
       files: {
-        copy: './toolkit/vscode-intellisense/**/*',
+        copy: {
+          nodeDir: [
+            './node_modules/phaser-ce/typescript/p2.d.ts',
+            './node_modules/phaser-ce/typescript/phaser.comments.d.ts',
+            './node_modules/phaser-ce/typescript/pixi.d.ts'
+          ],
+          jsConfig: './toolkit/vscode-intellisense/jsconfig.json',
+          default: './toolkit/vscode-intellisense/**/*'
+        },
         to: './src/js/'
       }
     }
--- gulp/tasks/vscode-intellisense.js
+++ gulp/tasks/vscode-intellisense.js
@@ -1,13 +1,29 @@
 'use strict';
 
 // Necessary Plugins
-var gulp    = require('gulp');
-var plumber = require('gulp-plumber');
-var paths   = require('../paths');
+const gulp       = require('gulp');
+const plumber    = require('gulp-plumber');
+const path       = require('path');
+const pathExists = require('path-exists');
+const paths      = require('../paths');
+
+const NODE_DIR = path.resolve(__dirname, '../../node_modules/');
 
 // Call VSCode Intellisense for Phaser
 module.exports = gulp.task('vscode', () => {
-  return gulp.src(paths.toolkit.vscodeIntellisense.files.copy)
-    .pipe(plumber())
-    .pipe(gulp.dest(paths.toolkit.vscodeIntellisense.files.to));
+  pathExists(NODE_DIR).then(exists => {
+    if (exists) {
+      gulp.src(paths.toolkit.vscodeIntellisense.files.copy.nodeDir)
+        .pipe(plumber())
+        .pipe(gulp.dest(`${paths.toolkit.vscodeIntellisense.files.to}/defs/`));
+      gulp.src(paths.toolkit.vscodeIntellisense.files.copy.jsConfig)
+        .pipe(plumber())
+        .pipe(gulp.dest(paths.toolkit.vscodeIntellisense.files.to));
+    }
+    else {
+      return gulp.src(paths.toolkit.vscodeIntellisense.files.copy.default)
+        .pipe(plumber())
+        .pipe(gulp.dest(paths.toolkit.vscodeIntellisense.files.to));
+    }
+  });
 });
