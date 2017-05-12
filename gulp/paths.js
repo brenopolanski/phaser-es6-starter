95897b4a7504804022a46b6f01ee282f737bec9c
brenopolanski <breno.polanski@gmail.com>
2017-05-11 22:24:17 -0300

    Rename folder for toolkit

--- gulp/paths.js
+++ gulp/paths.js
@@ -1,10 +1,10 @@
 'use strict';
 
 module.exports = {
-  configTemplates: {
+  toolkit: {
     vscodeIntellisense: {
       files: {
-        copy: './config-templates/vscode-intellisense/**/*',
+        copy: './toolkit/vscode-intellisense/**/*',
         to: './src/js/'
       }
     }
--- gulp/tasks/vscode-intellisense.js
+++ gulp/tasks/vscode-intellisense.js
@@ -7,7 +7,7 @@ var paths   = require('../paths');
 
 // Call VSCode Intellisense for Phaser
 module.exports = gulp.task('vscode', () => {
-  return gulp.src(paths.configTemplates.vscodeIntellisense.files.copy)
+  return gulp.src(paths.toolkit.vscodeIntellisense.files.copy)
     .pipe(plumber())
-    .pipe(gulp.dest(paths.configTemplates.vscodeIntellisense.files.to));
+    .pipe(gulp.dest(paths.toolkit.vscodeIntellisense.files.to));
 });
