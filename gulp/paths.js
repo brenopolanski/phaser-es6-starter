da3f48a83dbb5b5bf9221fab4f5d48025dce8fc5
brenopolanski <breno.polanski@gmail.com>
2017-05-25 14:13:05 -0300

    Update task template:empty

--- gulp/paths.js
+++ gulp/paths.js
@@ -1,6 +1,8 @@
 'use strict';
 
 module.exports = {
+  srcRoot: './src/**/*',
+
   toolkit: {
     vscodeIntellisense: {
       files: {
--- gulp/tasks/project-templates.js
+++ gulp/tasks/project-templates.js
@@ -1,20 +1,21 @@
 'use strict';
 
 // Necessary Plugins.
-const gulp       = require('gulp');
-const plumber    = require('gulp-plumber');
-const del        = require('del');
-const paths      = require('../paths');
+const gulp    = require('gulp');
+const plumber = require('gulp-plumber');
+const del     = require('del');
+const paths   = require('../paths');
 
+// Clear the `src/` folder.
 gulp.task('clean:src', () => {
-  return del('./src/**/*');
+  return del(paths.srcRoot);
 });
 
+// Call to create an empty game project template in `src/` folder.
 gulp.task('empty', ['clean:src'], () => {
   gulp.src(paths.toolkit.projectTemplates.empty.files.copy)
     .pipe(plumber())
     .pipe(gulp.dest(paths.toolkit.projectTemplates.empty.files.to));
 });
 
-// Call to create an empty game project template in `src/` folder.
 module.exports = gulp.task('template:empty', ['clean:src', 'empty']);
