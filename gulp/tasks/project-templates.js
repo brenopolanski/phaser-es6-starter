c82a67a48cb3988e34206056fc991f32980aa9dd
Breno Polanski <breno.polanski@gmail.com>
2017-05-24 22:17:57 -0300

    Update project-templates.js

--- gulp/tasks/project-templates.js
+++ gulp/tasks/project-templates.js
@@ -7,11 +7,16 @@ const del        = require('del');
 const vinylPaths = require('vinyl-paths');
 const paths      = require('../paths');
 
-// Call to create an empty game project template in `src/` folder.
-module.exports = gulp.task('template:empty', () => {
+gulp.task('clean:src', () => {
   gulp.src('./src/**/*')
     .pipe(vinylPaths(del));
-  gulp.src(paths.toolkit.projectTemplates.files.copy)
-    .pipe(plumber())
-    .pipe(gulp.dest(paths.toolkit.projectTemplates.files.to));
 });
+
+gulp.task('empty', ['clean:src'], () => {
+  gulp.src(paths.toolkit.projectTemplates.empty.files.copy)
+    .pipe(plumber())
+    .pipe(gulp.dest(paths.toolkit.projectTemplates.empty.files.to));
+});
+
+// Call to create an empty game project template in `src/` folder.
+module.exports = gulp.task('template:empty', ['empty']);
